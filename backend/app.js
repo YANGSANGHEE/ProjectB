b6d7143718a71d42490fd8b01d5d4b874b99bf63
YANGSANGHEE <13614024ysh@gmail.com>
2022-12-12 19:07:24 +0900

    express 세팅 완료

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express') // express
+const cors = require('cors')
+const router = require('./Router/ApiSet')
+const app = express();
+const Port = process.env.Port || 8080;
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", router);
+
+
+app.get('/', (req, res) => {
+  res.send(`server Start ${Port}`)
+}).listen(Port, (err) => {
+  if (err) throw err;
+  console.log(`server Start ${Port}`)
+})
