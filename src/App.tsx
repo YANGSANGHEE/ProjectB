a541ff089aa696d00b8d21b4717442652d0df03d
YANGSANGHEE <13614024ysh@gmail.com>
2022-12-14 10:37:09 +0900

    세팅 1차 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import '@/App.css';
 import LayOut from './Components/Layout';
+import styled from 'styled-components';
+import { calcPx } from './Hooks/CalcPx';
+
+const Test = styled.div`
+  width: ${calcPx(100)};
+  height: ${calcPx(100)};
+  background: ${({ theme }) => theme.Color.black};
+`;
 
 const App = () => {
   return (
     <>
       <LayOut />
+      <Test></Test>
     </>
   );
 };
--- src/Theme/theme.ts
+++ src/Theme/theme.ts
@@ -3,6 +3,7 @@ import { calcPx } from '@/Hooks/CalcPx';
 const fontSize = {
   font_11: `
     font-Size : 11px;
+    font-weight : 400;,
   `,
   font_12: `
   font-Size : 12px;
@@ -11,7 +12,7 @@ const fontSize = {
 
 const Color = {
   green_gradient: 'linear-gradient(90deg, #00A542 0%, #81E600 100%)',
-  black_: '#222222',
+  black: '#222222',
   gray: '#868686',
   l_gray: '#979797',
   W_gray: '#CECECE',
