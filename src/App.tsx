c1238cdde53dbf73f750ee6a17047284f0c46a5b
leeahyeon <leeahyeon01@gmail.com>
2022-12-14 17:51:01 +0900

    대전 주요 사건 화면에 띄우기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import '@/App.css';
 import LayOut from './Components/Layout';
 import styled from 'styled-components';
-import { calcPx } from './Hooks/CalcPx';
+import { calcPx } from './Hooks/CalcPx'; 
+import  TrafficNews  from './Components/TrafficNews/TrafficNews';
 
 const Test = styled.div`
   width: ${calcPx(100)};
@@ -13,8 +14,9 @@ const App = () => {
   return (
     <>
       <LayOut />
-      <img src='/img/CCTV.png' alt='CCTV'></img>\{/* 이미지 경로 참고 */}
+      <img src='/img/CCTV.png' alt='CCTV'></img>\{/* 이미지 경로 참고 */} 
       <Test />
+      <TrafficNews/>
       {/* styled component , calcPx적용 */}
     </>
   );
--- src/Components/TrafficNews/TrafficNews.tsx
+++ src/Components/TrafficNews/TrafficNews.tsx
@@ -2,7 +2,9 @@ import axios from "axios";
 import { it } from "node:test";
 import { useEffect, useState } from "react";
 const key = "480dc33ea22b4f79ac3ea6368e1c9eac"; 
-const url = `https://openapi.its.go.kr:9443/eventInfo?apiKey=${key}&type=all&eventType=all&minX=126.800000&maxX=127.890000&minY=34.900000 &maxY=35.100000&getType=json`;
+
+  const url = `https://openapi.its.go.kr:9443/eventInfo?apiKey=test&type=all&eventType=all&minX=127.252183&maxX=127.538356&minY==36.194005 &maxY=36.499218&getType=json`; 
+  //const url =`https://openapi.its.go.kr:9443/eventInfo?apiKey=${key}&type=all&eventType=all&minX=127.252183&maxX=127.538356&minY=36.194005 &maxY=36.499218&getType=json`;
 const TrafficNews = () => {   
 
 
@@ -23,30 +25,13 @@ const TrafficNews = () => {
 
 
 
-
-  
   console.log(news)
   
   
-//   for(let key in news){
-//     console.log(news[key]) 
-//     for(let keys in news[key]){ 
-//       getbodyValue(news[key][keys])
-//     }
-//   } 
-  
-// console.log(bodyValue)
-
-
   
   // 객체의 value값 으로 접근 
   const keys = Object.values(news)
   
-
- 
- 
-
-   
 
 
   
@@ -56,14 +41,22 @@ const TrafficNews = () => {
    <div>hello</div> 
      
    <>
-   {/* {news.map((item:any , index:number)=>{
+   {news.map((item:any , index:number)=>{
     console.log(item)
     return(
     
-   <div>{item}</div> 
+    <div>
+     type ={item.type} 
+    message = {item.message}
+    detailevent = {item.eventDetailType} 
+    event = {item.eventType}
+     
+
+    </div>
+
        
     )
-   })} */}
+   })}
    
    </>
 
