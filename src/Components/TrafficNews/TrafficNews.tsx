48de3c97a5b9d4cbb06c9821cb85a2beb898c701
leeahyeon <leeahyeon01@gmail.com>
2022-12-15 12:51:07 +0900

    대전 실시간 공사·사고

--- src/Components/TrafficNews/TrafficNews.tsx
+++ src/Components/TrafficNews/TrafficNews.tsx
@@ -1,21 +1,24 @@
 import axios from "axios";
 import { it } from "node:test";
 import { useEffect, useState } from "react";
+// const {kakao} = window;
 const key = "480dc33ea22b4f79ac3ea6368e1c9eac"; 
 
-  const url = `https://openapi.its.go.kr:9443/eventInfo?apiKey=test&type=all&eventType=all&minX=127.252183&maxX=127.538356&minY==36.194005 &maxY=36.499218&getType=json`; 
+//대전 실시간 사고·공사 api
+  const url = `https://openapi.its.go.kr:9443/eventInfo?apiKey=480dc33ea22b4f79ac3ea6368e1c9eac&type=all&eventType=all&minX=127.252183&maxX=127.538356&minY=36.194005&maxY=36.499218&getType=json`; 
   //const url =`https://openapi.its.go.kr:9443/eventInfo?apiKey=${key}&type=all&eventType=all&minX=127.252183&maxX=127.538356&minY=36.194005 &maxY=36.499218&getType=json`;
-const TrafficNews = () => {   
 
 
+  const TrafficNews = () => {   
    const [news, setnews] = useState([]);   
 
 
    useEffect(() => {
     const fatchData = async () => {
       try{
-        const res = await axios.get(url); 
-        setnews(res.data.body.items)
+        const res = await axios.get(url);  
+        // setnews(res.data.body.items)
+         setnews(res.data.body.items)
       }catch(e){
         console.log(e)
       }
@@ -23,44 +26,35 @@ const TrafficNews = () => {
     fatchData()
    },[]) 
 
-
-
   console.log(news)
-  
-  
-  
-  // 객체의 value값 으로 접근 
-  const keys = Object.values(news)
+  console.log("g")
   
 
-
   
-  const trafficAip = "https://openapi.its.go.kr:9443/eventInfo?apiKey=480dc33ea22b4f79ac3ea6368e1c9eac&type=all&eventType=all&minX=126.800000&maxX=127.890000&minY=34.900000 &maxY=35.100000&getType=json"
-  return <> 
+  return (
+    <> 
    
-   <div>hello</div> 
-     
-   <>
-   {news.map((item:any , index:number)=>{
-    console.log(item)
-    return(
+    <div>hello</div> 
     
-    <div>
-     type ={item.type} 
-    message = {item.message}
-    detailevent = {item.eventDetailType} 
-    event = {item.eventType}
-     
-
-    </div>
-
-       
-    )
-   })}
+    //대전 뉴스·공사 
+    {news.map((item:any , index:number)=>{
+     console.log(item)
+     return(
    
-   </>
-
-  </>;
+     <div className="news_div">
+     {item.roadName}
+     {item.message}
+     </div>   
+     )
+    })}
+    
+    </>
+ 
+   
+    
+  )
+  
+ 
 };
 
 export default TrafficNews;
