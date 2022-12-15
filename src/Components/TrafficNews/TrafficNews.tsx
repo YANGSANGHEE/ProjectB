db0559119367b16376b4683da5b38acce85d944f
leeahyeon <leeahyeon01@gmail.com>
2022-12-15 16:24:29 +0900

    슬라이드 움직임 구현

--- src/Components/TrafficNews/TrafficNews.tsx
+++ src/Components/TrafficNews/TrafficNews.tsx
@@ -1,12 +1,15 @@
-import axios from "axios";
-import { it } from "node:test";
+import axios from "axios"; 
 import { useEffect, useState } from "react";
+import styled,{ keyframes } from "styled-components";
 // const {kakao} = window;
+import Div from "./div"
+// import {keyframes} from "@emotion/react"
 const key = "480dc33ea22b4f79ac3ea6368e1c9eac"; 
 
 //대전 실시간 사고·공사 api
   const url = `https://openapi.its.go.kr:9443/eventInfo?apiKey=480dc33ea22b4f79ac3ea6368e1c9eac&type=all&eventType=all&minX=127.252183&maxX=127.538356&minY=36.194005&maxY=36.499218&getType=json`; 
   //const url =`https://openapi.its.go.kr:9443/eventInfo?apiKey=${key}&type=all&eventType=all&minX=127.252183&maxX=127.538356&minY=36.194005 &maxY=36.499218&getType=json`;
+
 
 
   const TrafficNews = () => {   
@@ -27,31 +30,65 @@ const key = "480dc33ea22b4f79ac3ea6368e1c9eac";
    },[]) 
 
   console.log(news)
-  console.log("g")
-  
+ 
+
+  const move = keyframes`
+    0%{
+      transform: translateX(50px);
+    } 
+    100%{
+      transform: translateX(-200px);
+    }
+  `
+
+  const Digit:any = styled.div`
+  border: solid 2px red; 
+  width: 500px;
+  height: 40px;
+  display: flex; 
+  `
+
+
+
+  const StyledDiv:any = styled.div`
+    width: 200px;
+    height: 20px;
+    border: solid 2px blue; 
+    animation: ${move} 2s linear infinite;
+    `  
+
+
+
+
 
   
-  return (
+  return (  
     <> 
-   
-    <div>hello</div> 
+    {/* 대전 뉴스·공사  */}
     
-    //대전 뉴스·공사 
-    {news.map((item:any , index:number)=>{
+
+     <Digit>
+     {/* <StyledDiv>hello</StyledDiv> */}
+      
+
+     {news.map((item:any , index:number)=>{
      console.log(item)
-     return(
-   
-     <div className="news_div">
+     return( 
+     <> 
+      <StyledDiv>  
      {item.roadName}
-     {item.message}
-     </div>   
+     {item.message}</StyledDiv>
+      </>
+     
      )
-    })}
-    
-    </>
- 
-   
-    
+    })}     
+  
+
+
+     </Digit>
+  
+
+    </> 
   )
   
  
