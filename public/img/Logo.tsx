cf2b1f43c29ef604ffc597c89d8a6f80eb6a0652
jeonhyoungmin <jhmin7509@gmail.com>
2022-12-16 16:03:27 +0900

    머전패스 logo tsx화 및 export

---
+++ public/img/Logo.tsx
@@ -0,0 +1,153 @@
+const logo = () => {
+  return (
+    <svg
+      width="126"
+      height="37"
+      viewBox="0 0 126 37"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <path
+          d="M21.5742 1.94112L13.891 24.8394H0L7.68317 1.94112H21.5742ZM5.62343 21.3738H10.5971L15.9548 5.40671H10.9812L5.62343 21.3738ZM22.6899 25.1255H18.2312L21.9584 14.0176L17.9696 15.2395L19.163 11.6881L23.1517 10.4662L26.2004 1.37714H30.6591L22.6899 25.1255Z"
+          fill="url(#paint0_linear_86_202)"
+        />
+        <path
+          d="M37.0428 17.5977L34.0472 27.7616L113.948 28.0722L112.734 32.208H26.4744L31.3826 17.5977H37.0428ZM50.9379 1.65925L49.7732 5.12484H43.9781L43.1403 7.62596C43.0627 7.85482 42.9809 8.07551 42.8869 8.29211C42.7929 8.50871 42.6949 8.72531 42.5886 8.92965L47.6481 12.7345L44.5053 15.7464L39.8545 12.2522C37.8602 13.8992 35.0812 15.1048 31.5134 15.8609L30.6592 12.7345C31.779 12.5056 32.7762 12.2522 33.6589 11.9661C34.5417 11.6842 35.3141 11.3409 35.9762 10.9444C36.6382 10.548 37.1981 10.074 37.6477 9.52224C38.0972 8.97461 38.4446 8.32889 38.6939 7.58918L39.5235 5.11667H34.0063L35.1711 1.65107H50.9379V1.65925ZM58.4208 1.37317L52.7279 18.3334H48.2692L51.1668 9.69797L47.3497 10.4949L48.5431 6.94347L52.3601 6.14654L53.9621 1.37317H58.4208Z"
+          fill="url(#paint1_linear_86_202)"
+        />
+        <path
+          d="M68.5069 23.4664H54.0479L55.2126 20.0008H57.2029L62.5606 4.03366H60.5704L61.7351 0.568058H76.1942L75.0295 4.03366H73.0392L67.6814 20.0008H69.6717L68.5069 23.4664ZM61.3755 20.0008H63.5047L68.8625 4.03366H66.7333L61.3755 20.0008ZM69.5491 23.7484L77.5183 0H81.6092L78.2335 10.0576L81.0289 9.17485L84.1062 0H88.2257L80.2565 23.7484H76.137L79.7415 13.0083L76.9462 13.891L73.6399 23.7484H69.5491Z"
+          fill="url(#paint2_linear_86_202)"
+        />
+        <path
+          d="M104.949 2.05573L103.755 5.60715C103.298 6.97214 102.681 8.24721 101.9 9.44055L109.804 14.8392L106.567 17.9656L98.9576 12.7958C96.0478 15.3133 92.0468 17.1319 86.9587 18.2517L86.3784 14.9863C90.0238 14.2875 92.8764 13.1636 94.9362 11.6188C96.9959 10.074 98.4426 8.0633 99.2763 5.58262L100.457 2.0598H104.949V2.05573Z"
+          fill="url(#paint3_linear_86_202)"
+        />
+        <path
+          d="M112.133 36.9611H86.4766L87.3756 34.1494H113.033L112.133 36.9611Z"
+          fill="url(#paint4_linear_86_202)"
+        />
+        <path
+          d="M95.1161 23.4949L94.0127 26.3067H117.197L118.382 23.4949H95.1161Z"
+          fill="url(#paint5_linear_86_202)"
+        />
+        <path
+          d="M120.013 23.4949H118.885L117.72 26.3067H118.836L120.013 23.4949Z"
+          fill="url(#paint6_linear_86_202)"
+        />
+        <path
+          d="M121.872 23.4949H120.589L119.465 26.3067H120.732L121.872 23.4949Z"
+          fill="url(#paint7_linear_86_202)"
+        />
+        <path
+          d="M126 23.4949H122.203L121.063 26.3067H124.925L126 23.4949Z"
+          fill="url(#paint8_linear_86_202)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_86_202"
+          x1="9.7119"
+          y1="1.37713"
+          x2="7.07267"
+          y2="32.3612"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop />
+          <stop offset="1" stop-color="#363636" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_86_202"
+          x1="54.1834"
+          y1="1.37317"
+          x2="52.615"
+          y2="41.834"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop />
+          <stop offset="1" stop-color="#363636" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_86_202"
+          x1="64.8744"
+          y1="-2.27881e-06"
+          x2="62.5035"
+          y2="31.0277"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop />
+          <stop offset="1" stop-color="#363636" />
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_86_202"
+          x1="93.7989"
+          y1="2.05572"
+          x2="92.19"
+          y2="23.2173"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop />
+          <stop offset="1" stop-color="#363636" />
+        </linearGradient>
+        <linearGradient
+          id="paint4_linear_86_202"
+          x1="94.8887"
+          y1="34.1494"
+          x2="94.8457"
+          y2="37.8438"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop />
+          <stop offset="1" stop-color="#363636" />
+        </linearGradient>
+        <linearGradient
+          id="paint5_linear_86_202"
+          x1="101.732"
+          y1="23.4949"
+          x2="101.685"
+          y2="27.1894"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop />
+          <stop offset="1" stop-color="#363636" />
+        </linearGradient>
+        <linearGradient
+          id="paint6_linear_86_202"
+          x1="118.446"
+          y1="23.4949"
+          x2="117.957"
+          y2="27.1239"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop />
+          <stop offset="1" stop-color="#363636" />
+        </linearGradient>
+        <linearGradient
+          id="paint7_linear_86_202"
+          x1="120.228"
+          y1="23.4949"
+          x2="119.761"
+          y2="27.13"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop />
+          <stop offset="1" stop-color="#363636" />
+        </linearGradient>
+        <linearGradient
+          id="paint8_linear_86_202"
+          x1="122.627"
+          y1="23.4949"
+          x2="122.397"
+          y2="27.1755"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop />
+          <stop offset="1" stop-color="#363636" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default logo;
