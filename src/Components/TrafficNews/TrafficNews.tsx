990fb338b41429654dccefbae168e76456a798dd
leeahyeon <leeahyeon01@gmail.com>
2022-12-16 16:05:22 +0900

    속보 수정중

--- src/Components/TrafficNews/TrafficNews.tsx
+++ src/Components/TrafficNews/TrafficNews.tsx
@@ -7,11 +7,10 @@ const key = "480dc33ea22b4f79ac3ea6368e1c9eac";
 //대전 실시간 사고·공사 api
   const url = `https://openapi.its.go.kr:9443/eventInfo?apiKey=480dc33ea22b4f79ac3ea6368e1c9eac&type=all&eventType=all&minX=127.252183&maxX=127.538356&minY=36.194005&maxY=36.499218&getType=json`; 
 
-  // `https://openapi.its.go.kr:9443/eventInfo?apiKey=${ITS}=all&eventType=all&minX=127.252183&maxX=127.538356&minY=36.194005&maxY=36.499218&getType=json`
+ 
 
   const TrafficNews = () => {    
-   
-  //  const ITS = process.env.REACT_APP_ITS_KEY2;
+  // const ITS = process.env.REACT_APP_ITS_KEY2;
    const [news, setnews] = useState([]);   
 
    useEffect(() => {
@@ -73,20 +72,16 @@ const key = "480dc33ea22b4f79ac3ea6368e1c9eac";
     align-content: center;
     justify-content: center;
     align-items: center;
-
-
     &>img{
     width: 80px;
     width: 30px;
     height: 30px;
   }
-
   `
 
   const StyledDiv:any = styled.div`
   margin-right: 10px;
   border: solid 2px blue; 
-
   animation: ${move} 4s linear infinite;
     `  
   
@@ -94,9 +89,7 @@ const key = "480dc33ea22b4f79ac3ea6368e1c9eac";
   
   return (  
     <> 
-     <Digit>
-     {/* <StyledDiv>hello</StyledDiv> */}
-       
+     <Digit>       
        <Siren> <img className="img" src="/img/Siren.png" alt='siren'></img></Siren>
        <Wrapdiv>
      {news.map((item:any , index:number)=>{
@@ -112,12 +105,7 @@ const key = "480dc33ea22b4f79ac3ea6368e1c9eac";
      )
     })}
       </Wrapdiv>      
-  
-
-
      </Digit>
-  
-
     </> 
   )
   
