84cb515188cec1d480a5731353301d73f6a42ac7
YANGSANGHEE <13614024ysh@gmail.com>
2022-12-16 18:14:03 +0900

    혼잡도 팝업 제작중

--- src/Components/AxiosExample.tsx
+++ src/Components/AxiosExample.tsx
@@ -4,32 +4,32 @@ import { useEffect } from 'react';
 
 // console.log(ITS);
 const AxiosExample = () => {
-//   //컴포넌트가 처음 나타났을때
-//   useEffect(() => {
-//     // axios는 Promise 기반
-//     axios
-//       .get(`http://localhost:8080/test`) // API url 입력
-//       .then((res) => console.log(res)) // axios는 default가 JSON으로 값을 받아옴
-//       .catch((e: ErrorCallback) => {
-//         if (e) throw e;
-//         console.log('에러');
-//       }); //에러처리
-//   }, []);
+  //   //컴포넌트가 처음 나타났을때
+  //   useEffect(() => {
+  //     // axios는 Promise 기반
+  //     axios
+  //       .get(`http://localhost:8080/test`) // API url 입력
+  //       .then((res) => console.log(res)) // axios는 default가 JSON으로 값을 받아옴
+  //       .catch((e: ErrorCallback) => {
+  //         if (e) throw e;
+  //         console.log('에러');
+  //       }); //에러처리
+  //   }, []);
 
-//   const Post = async (): Promise<void> => {
-//     await axios
-//       .post('http://localhost:8080/post/testpost', {
-//         id: '그냥보냄',
-//         password: '1sfasd351243s',
-//       })
-//       .then(() => {
-//         alert('성공!');
-//       })
-//       .catch((e: ErrorCallback) => {
-//         if (e) throw e;
-//         console.log('에러');
-//       });
-//   };
+  //   const Post = async (): Promise<void> => {
+  //     await axios
+  //       .post('http://localhost:8080/post/testpost', {
+  //         id: '그냥보냄',
+  //         password: '1sfasd351243s',
+  //       })
+  //       .then(() => {
+  //         alert('성공!');
+  //       })
+  //       .catch((e: ErrorCallback) => {
+  //         if (e) throw e;
+  //         console.log('에러');
+  //       });
+  //   };
   return (
     <>
       <h1>Axios 테스트</h1>
