7858ea638f4ed2d6f778bb28dddcf738dca90620
YANGSANGHEE <13614024ysh@gmail.com>
2022-12-19 17:58:51 +0900

    CCTV APPCSS 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import "@/App.css";
-import LayOut from "./Components/Layout";
-import styled from "styled-components";
-import { calcPx } from "./Hooks/CalcPx";
-import CCTV from "./Components/CCTV/CCTV";
-import CCTVALL from "./Components/CCTV/CCTVAll";
+import '@/App.css';
+import LayOut from './Components/Layout';
+import styled from 'styled-components';
+import { calcPx } from './Hooks/CalcPx';
+import CCTV from './Components/CCTV/CCTV';
 
 const Test = styled.div`
   width: ${calcPx(100)};
@@ -15,7 +14,7 @@ const App = () => {
   return (
     <>
       <LayOut />
-      <img src="/img/CCTV.png" alt="CCTV"></img>\{/* 이미지 경로 참고 */}
+      <img src='/img/CCTV.png' alt='CCTV'></img>\{/* 이미지 경로 참고 */}
       {/* <Test /> */}
       {/* styled component , calcPx적용 */}
       <CCTV />
--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,22 +1,5 @@
-import AxiosExample from "./AxiosExample";
-import styled from "styled-components";
-
-const Media = styled.div`
-  width: 100px;
-  height: 100px;
-  ${({ theme }) => theme.device.mobile} {
-    background-color: salmon;
-  }
-  background-color: red;
-`;
-
 const LayOut = () => {
-  return (
-    <>
-      <Media />
-      <AxiosExample />
-    </>
-  );
+  return <></>;
 };
 
 export default LayOut;
