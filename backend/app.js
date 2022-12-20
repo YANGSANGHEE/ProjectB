24c7fb75dfc8f95e062e738cfcd8287c2019fb36
KinGeunSoo <geunsoo1106@gmail.com>
2022-12-20 09:51:32 +0900

    전형민 팀원 도움으로 cctv router 서버 구현

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,29 @@
-const express = require('express') // express
-const cors = require('cors')
-const router = require('./Router/ApiSetGET')
-const postrouter = require('./Router/ApiSetPOST')
+const express = require("express"); // express
+const cors = require("cors");
+const router = require("./Router/ApiSetGET");
+const postrouter = require("./Router/ApiSetPOST");
+const cctvrouter = require("./Router/CctvSetGET");
 const app = express();
 const Port = process.env.Port || 8080;
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", router);
-app.use('/post', postrouter)
+app.use("/post", postrouter);
 
-app.get('/', (req, res) => {
-  res.send(`server Start ${Port}`)
-}).listen(Port, (err) => {
-  if (err) throw err;
-  console.log(`server Start ${Port}`)
-})
+app.get("/cctv", cctvrouter);
+app
+  .get("/", (req, res) => {
+    res.send(`server Start ${Port}`);
+  })
+  .listen(Port, err => {
+    if (err) throw err;
+    console.log(`server Start ${Port}`);
+  });
