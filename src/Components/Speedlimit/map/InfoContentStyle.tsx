1c313f35588bc60164c5a547053357338fab7301
cho <lll0858630@gmail.com>
2022-12-21 18:32:25 +0900

    인포윈도우 css 작업중

---
+++ src/Components/Speedlimit/map/InfoContentStyle.tsx
@@ -0,0 +1,20 @@
+import { useCallback, useEffect, useState, useRef } from 'react'
+const { kakao } = window; //불러오기에 문제없음
+// import vslData from "../testData/vsl.json" //api 복사한 json데이터
+import Refresh from '@/Common/Refresh';
+import StylePopup from './Enforcement';
+import enfCamData from "../testData/unmanned_enforcement_cam_dajeon.json"
+
+const InfoContentStyle = {
+  // const displaySet =(qr:string, display:string, flexDirection:string, alignItems:string) =>{
+  //   let getQr:any = document.querySelectorAll<HTMLElement>(qr);
+  //   getQr.style.display = display;
+  //   getQr.style.flexDirection = flexDirection;
+  //   getQr.style.alignItems = alignItems;
+  // }
+}
+// bgColor: ,
+// borderSet: ,
+// font: ,
+
+export default InfoContentStyle;
--- src/Components/Speedlimit/map/SpeedMarker_cluster_enfCam.tsx
+++ src/Components/Speedlimit/map/SpeedMarker_cluster_enfCam.tsx
@@ -4,6 +4,7 @@ const { kakao } = window; //불러오기에 문제없음
 import Refresh from '@/Common/Refresh';
 import StylePopup from './Enforcement';
 import enfCamData from "../testData/unmanned_enforcement_cam_dajeon.json"
+import InfoContentStyle from './InfoContentStyle';
 /**
  * ? 대전 교통 단속카메라 기준
  * 줌 인 or 아웃에 따라 마커가 그룹으로 표시됨(클러스터)
@@ -17,9 +18,10 @@ const MarkerClusterEnfCam = () => {
 
   const map_def = useRef({});
   const getCenter_def = useRef();
-  
+
   // console.log(enfCamData.map(val => {return val["제한속도"]}))
   let datas = enfCamData;
+  let arr: any = [];
   useEffect(() => {
     //* 요청 횟수 문제로 테스트 데이터 사용
     new Promise((resolve, rejects) => {
@@ -51,71 +53,107 @@ const MarkerClusterEnfCam = () => {
           minLevel: 6 // 클러스터 할 최소 지도 레벨 
         });
         // 마커
+        let arr: any[] = [];
         const markers = enfCamData.map((value: any) => {
-            let imgSrc;
-            let speed:number = value["제한속도"];
-            //속도에 따라 표시되는 마커 이미지 
-            switch (speed) {
-              case 20:
-                imgSrc = "/img/limit_20.png";
-                break;
-              case 30:
-                imgSrc = "/img/limit_30.png";
-                break;
-              case 40:
-                imgSrc = "/img/limit_40.png";
-                break;
-              case 50:
-                imgSrc = "/img/limit_50.png";
-                break;
-              case 60:
-                imgSrc = "/img/limit_60.png";
-                break;
-              case 70:
-                imgSrc = "/img/limit_70.png";
-                break;
-              case 80:
-                imgSrc = "/img/limit_80.png";
-                break;
-              case 100:
-                imgSrc = "/img/limit_100.png";
-                break;
-              case 110:
-                imgSrc = "/img/limit_110.png";
-                break;
-              default:
-                imgSrc = "/img/limit_0.png" //투명 이미지
-            };
-            let imgSize = new kakao.maps.Size(60 * 0.5, 73 * 0.5); //마커 사이즈
-            let markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize);
-            let positions = new kakao.maps.LatLng(value["위도"], value["경도"]); //마커가 표시될 좌표
-            let marker = new kakao.maps.Marker({
-              position: positions,
-              image: markerImg, //마커이미지 
+          let imgSrc;
+          let speed: number = value["제한속도"];
+          //속도에 따라 표시되는 마커 이미지 
+          switch (speed) {
+            case 20:
+              imgSrc = "/img/limit_20.png";
+              break;
+            case 30:
+              imgSrc = "/img/limit_30.png";
+              break;
+            case 40:
+              imgSrc = "/img/limit_40.png";
+              break;
+            case 50:
+              imgSrc = "/img/limit_50.png";
+              break;
+            case 60:
+              imgSrc = "/img/limit_60.png";
+              break;
+            case 70:
+              imgSrc = "/img/limit_70.png";
+              break;
+            case 80:
+              imgSrc = "/img/limit_80.png";
+              break;
+            case 100:
+              imgSrc = "/img/limit_100.png";
+              break;
+            case 110:
+              imgSrc = "/img/limit_110.png";
+              break;
+            default:
+              imgSrc = "/img/limit_0.png" //투명 이미지
+          };
+          let imgSize = new kakao.maps.Size(60 * 0.5, 73 * 0.5); //마커 사이즈
+          let markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize);
+          let positions = new kakao.maps.LatLng(value["위도"], value["경도"]); //마커가 표시될 좌표
+          let marker = new kakao.maps.Marker({
+            position: positions,
+            image: markerImg, //마커이미지 
+          });
+          /* 마커 마우스오버 이벤트 */
+          // 마커에 표시할 인포윈도우를 생성 
+          if (value["제한속도"] !== 0) {
+            let infowindow = new kakao.maps.InfoWindow({
+              // content: `<div style="
+              // width: fit-content;
+              // padding: 2%;
+              // ">테스트<span style="
+              // font-size: 10pt; 
+              // text-align: center;
+              // "}>${value["설치장소"]}</span></div>`,
+              content: `
+              <div class="Wrap">
+                <p class="descCon">
+                  <span class="title"> 소재지도로명 | </span>
+                  <span class="desc">${value["소재지도로명주소"]}</span>
+                </p>
+                <p class="descCon"> 
+                  <span class="title"> 설치장소 | </span>
+                  <span class="desc">${value["설치장소"]}</span>
+                </p>
+              </div>`,
+              removable: true
             });
 
-            /* 마커 마우스오버 이벤트 */
-            // 마커에 표시할 인포윈도우를 생성 
-          if(value["제한속도"] !== 0){
-            let infowindow = new kakao.maps.InfoWindow({
-              content: `<div style="
-              width: fit-content;
-              padding: 2%;
-              "><span style="
-              font-size: 10pt; 
-              text-align: center;
-              "}>${value["설치장소"]}</span></div>` // 
+            const infoStyles =()=> {
+              let infowinDefault = infowindow.a;
+              let Wrap:any = document.querySelector<HTMLElement>(".Wrap");
+              let descCon:any = document.querySelector<HTMLElement>(".descCon");
+              let title:any = document.querySelector<HTMLElement>("span.title");
+              let desc:any = document.querySelector<HTMLElement>(".desc");
+
+              infowinDefault.style.backgroundColor = "#FFF";
+              infowinDefault.style.border = "none";
+              infowinDefault.style.borderRadius = "5px";
+              infowinDefault.style.opacity = "80%";
+              infowinDefault.style.padding = "2%";
+              infowinDefault.children[0].style.background = "none";
+
+              Wrap.style.display = "flex";
+              Wrap.style.flexDirection = "column";
+              // Wrap.style.alignItems = "center";
+              Wrap.style.padding = "2%";
+
+              title.style.fontWeight = "600";
+            }
+            
+            arr.push(infowindow);
+            const CloseEvent = () => {
+              arr.map((value, index) => arr[index].close());
+            };
+            kakao.maps.event.addListener(marker, 'click', function () {
+              CloseEvent();
+              infowindow.open(map, marker);
+              infoStyles();
+              console.dir(infowindow.a.children[2]);
             });
-            (function (marker, infowindow) {
-              // 마커에 mouseover 이벤트를 등록하고 마우스 오버 시 인포윈도우를 표시
-              kakao.maps.event.addListener(marker, 'mouseover', function () {
-                infowindow.open(map, marker);
-              });
-              // 마커에 mouseout 이벤트를 등록하고 마우스 아웃 시 인포윈도우를 닫기
-              kakao.maps.event.addListener(marker, 'mouseout', function () {
-                infowindow.close();
-              });
-            })(marker, infowindow)}
+          }
             return marker; //markers의 return 
         });
         //클러스터러에 마커 추가 & 마커 표시
