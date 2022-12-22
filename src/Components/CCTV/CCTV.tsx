6eb6343ee5b2d9b9e80d611c73bb059cdd187659
KinGeunSoo <geunsoo1106@gmail.com>
2022-12-22 10:08:32 +0900

    로딩창에는 경찰청 제공 안내문 삭제

--- src/Components/CCTV/CCTV.tsx
+++ src/Components/CCTV/CCTV.tsx
@@ -79,7 +79,7 @@ const CCTV = () => {
           let iwContent = `<iframe title="CCTV" width="320" height="280" style="border: none" src="${el.url}"></iframe><div style="font-size:5px;background-color:black;color:#fff">경찰청(UTIC)(LIVE)제공</div>`;
           // 영상 띄워주는 텍스트가 담겨있는 변수
 
-          let loadingContent = `<div style="display:flex; flex-direction:column; justify-content:center; align-items:center"><iframe title="CCTV" width="325" height="285" style="border: none; padding-left:20px;" src="/img/Podori_Loading.png"></iframe><p style="text-align:center; font-size:2rem">데이터를 불러오는 중 입니다...</p></div><div style="font-size:5px;background-color:black;color:#fff">경찰청(UTIC)(LIVE)제공</div>`;
+          let loadingContent = `<div style="display:flex; flex-direction:column; justify-content:center; align-items:center"><iframe title="CCTV" width="325" height="285" style="border: none; padding-left:20px;" src="/img/Podori_Loading.png"></iframe><p style="text-align:center; font-size:2rem">데이터를 불러오는 중 입니다...</p></div>`;
           // 로딩창 텍스트 변수
 
           const infowindow = new window.kakao.maps.InfoWindow({
